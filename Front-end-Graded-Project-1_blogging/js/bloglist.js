29f7b6a542824a53b366d05822f576033a8d930d
avas27 <abhishekvermasn27@gmail.com>
2019-07-09 21:54:29 +0530

    Now two posts on a single-line appears

--- Front-end-Graded-Project-1_blogging/js/bloglist.js
+++ Front-end-Graded-Project-1_blogging/js/bloglist.js
@@ -1,8 +1,10 @@
+//it displays delete modal when user clicks TRASH-icon on each post
 function deletePost()
-{
-    document.getElementsByClassName("containerDelete")[0].style.display="block";
-}
+        {
+            document.getElementsByClassName("containerDelete")[0].style.display="block";
+        }
 
+//it hides delete modal when user clicks "no" option on delete modal/dialog-box
 let noButton=document.getElementById("no");
 noButton.onclick=function closeDeletePost()
                     {
