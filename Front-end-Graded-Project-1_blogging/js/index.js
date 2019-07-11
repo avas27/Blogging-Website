82d745e834c4b741d4c99f8cf8553ff0db5ba4f2
avas27 <abhishekvermasn27@gmail.com>
2019-07-11 10:48:01 +0530

    Final Project-Work >>meets Given upGrad Guidlines

--- Front-end-Graded-Project-1_blogging/js/index.js
+++ Front-end-Graded-Project-1_blogging/js/index.js
@@ -1,5 +1,17 @@
 var allPostButton=document.getElementById("allPost");
+var createPost=document.getElementById("createPostButton");
+
 allPostButton.onclick=function() //redirects to bloglist-page when user clicks on all post button present on index page
                         {
                             window.open("html/bloglist.html");
+                        }
+
+createPost.onclick=function() //shows Create Post modal when user clicks on Create Post button 
+                        {  
+                            let createPostClose=document.getElementById("close3");
+                            document.getElementsByClassName("container")[2].style.display="block";
+                            createPostClose.onclick=function()
+                            {
+                                document.getElementsByClassName("container")[2].style.display="none";  
+                            }
                         }
